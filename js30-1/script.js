b3783eb3f64afcd4276ec96f01914b7f96cdb5de
Yana Malakhova <ya6ka0@gmail.com>
2023-04-04 17:41:40 +0300

    fix: implement correct audio playing

---
+++ js30-1/script.js
@@ -0,0 +1,62 @@
+const bgImage = document.querySelector('.main-container'),
+      logo = document.querySelector('.logo'),
+      playBtn = document.querySelector('.button'),
+      songs = document.querySelectorAll('.audio'),
+      navList = document.querySelector('.nav-list'),
+      navLinks = document.querySelectorAll('.nav-link');
+
+let isPlay = false;
+
+// bgImage.style.backgroundImage = `url("assets/img/bird_00.jpg")`;
+
+function playMusic(audioTracks, i = 5) {
+    audioTracks[i].play();
+    isPlay = true;
+    changePlayBtn();
+}
+
+function stopMusic() {
+    songs.forEach(song => {
+        song.pause();
+        song.currentTime = 0;
+    });
+
+    isPlay = false;
+    changePlayBtn();
+}
+
+function changePlayBtn() {
+    if (!isPlay) {
+        playBtn.classList.remove('pause');
+    } else {
+        playBtn.classList.add('pause');
+    }
+
+    console.log(`isPlay ${isPlay}`);
+}
+
+playBtn.addEventListener('click', (event) => {
+    if (!isPlay) {
+        playMusic(songs);
+    } else {
+        stopMusic();
+    }
+});
+
+navList.addEventListener('click', (event) => {
+    if (event.target && event.target.classList.contains('nav-link')) {
+        navLinks.forEach((link, i) => {
+            if (event.target == link) {
+                bgImage.style.backgroundImage = `url("assets/img/bird_0${i + 1}.jpg")`;
+                stopMusic();
+                playMusic(songs, i);
+            }
+        });
+    }
+});
+
+logo.addEventListener('click', () => {
+    bgImage.style.backgroundImage = `url("assets/img/bird_00.jpg")`;
+    stopMusic();
+    playMusic(songs);
+});
